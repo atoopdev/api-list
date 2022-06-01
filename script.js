808345e5b34e324c6d89b55191e3bd0f989fa057
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-06-01 10:25:45 -0700

    get list of all apis and output to screen

---
+++ script.js
@@ -0,0 +1,31 @@
+apiHTML = ""
+
+async function getApis(){
+    const response = await fetch(`https://api.publicapis.org/entries`)
+    if(!response.ok){
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message)
+    }
+    let data = await response.json()
+    console.log(data)
+    return data
+}
+
+getApis().then(apis =>
+    {
+        apiHTML = apis.entries.map(getApiHTML).join('')
+        document.getElementById("container").innerHTML = apiHTML
+        
+    })
+
+function getApiHTML(obj){
+    return `
+    <div class="api">
+    <div class="api-name"><a class="api-link" href="${obj.Link}">${obj.API}</a></div>
+    <div class="api-category">${obj.Category}</div>
+    <div class="api-description">${obj.Description}</div>
+    <div class="api-auth">Auth: ${obj.Auth}</div>
+    <div class="api-https">HTTPS: ${obj.HTTPS}</div>
+    </div>
+    `
+}
